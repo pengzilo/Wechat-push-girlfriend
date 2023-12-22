ff75500e25fde198a4da2081f36ba77144876ba5
xiaopao6 <115396330+xiaopao6@users.noreply.github.com>
2023-12-22 22:36:30 +0800

    Update WechatConfig.java

--- src/main/java/com/pt/vx/config/WechatConfig.java
+++ src/main/java/com/pt/vx/config/WechatConfig.java
@@ -39,7 +39,7 @@ public class WechatConfig {
     static {
         userList.add(getUser(
                 "oFTEU6VJS1Othyvu0Cqvu9_bYihY", //扫码关注你的测试号以后，测试平台会出现TA的微信号
-                "pz1kkzThNGObbyWUyKsaMM0O7yyQsvFhBA-F6ao1nRU", //要给这个人发送的模板ID
+                "chiw_kafk_d88JdlRGcq62XVzt502w8CUzejYn1Xakg", //要给这个人发送的模板ID
                 "棚子", //咋称呼这个人
                 "广西柳州鱼峰区", //这个人的详细地址
                 "柳州", //这个人在的城市
